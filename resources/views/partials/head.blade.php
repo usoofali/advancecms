4732a08efc1eba4a14500dbf7f23d130efbb46e1
Aliyu Yusuf <usoofali@gmail.com>
2026-03-21 17:40:58 +0100

    QWERT

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -2,7 +2,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
 <title>
-    {{ filled($title ?? null) ? $title.' - '.config('app.name', 'Laravel') : config('app.name', 'Laravel') }}
+    {{ filled($title ?? null) ? $title . ' - ' . config('app.name', 'Laravel') : config('app.name', 'Laravel') }}
 </title>
 
 <link rel="icon" href="/favicon.ico" sizes="any">
@@ -12,5 +12,10 @@
 <link rel="preconnect" href="https://fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
-@vite(['resources/css/app.css', 'resources/js/app.js'])
+@if (file_exists(public_path('build/manifest.json')))
+    <link rel="stylesheet" href="{{ asset('build/assets/app-BBy4e1YI.css') }}">
+    <script type="module" src="{{ asset('build/assets/app-l0sNRNKZ.js') }}"></script>
+@else
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+@endif
 @fluxAppearance

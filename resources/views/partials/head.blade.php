577452df61c01d5438b4240b308b91c9a531cb3b
Aliyu Yusuf <usoofali@gmail.com>
2026-04-07 15:40:10 +0100

    Added Just

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -13,7 +13,7 @@
 <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
 
 @if (file_exists(public_path('build/manifest.json')))
-    <link rel="stylesheet" href="{{ asset('build/assets/app-BBy4e1YI.css') }}">
+    <link rel="stylesheet" href="{{ asset('build/assets/app-Cnqnnf2g.css') }}">
     <script type="module" src="{{ asset('build/assets/app-l0sNRNKZ.js') }}"></script>
 @else
     @vite(['resources/css/app.css', 'resources/js/app.js'])

05bdb40291e9300799f70c1776552268664f45a0
Aliyu Yusuf <usoofali@gmail.com>
2026-07-21 15:28:49 +0100

    Roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             RbacSeeder::class,
+            UserSeeder::class,
         ]);
     }
 }
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $superAdmin = User::firstOrCreate(
+            ['email' => 'admin@advancecms.com'],
+            [
+                'name' => 'Super Administrator',
+                'password' => Hash::make('password'),
+                'email_verified_at' => now(),
+            ]
+        );
+
+        $role = Role::where('role_name', 'Super Admin')->first();
+
+        if ($role && ! $superAdmin->hasRole('Super Admin')) {
+            $superAdmin->roles()->attach($role->id);
+        }
+    }
+}
